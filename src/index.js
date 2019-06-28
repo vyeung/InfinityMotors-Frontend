4adff518f5fdbd4d37a9450579d0b0663f63b16b
vyeung <vl.yeung@yahoo.com>
2019-06-28 17:03:13 -0400

    add BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+
+const AppWithRoutes = (props) => {
+  return(
+    <BrowserRouter>
+      <App {...props} />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<AppWithRoutes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
