7088b396652a02ab488bc6e029c3beb64ef03df1
vyeung <vl.yeung@yahoo.com>
2019-06-28 17:05:38 -0400

    add Home route to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import "./App.css";
+
+import Layout from "../src/hoc/layout";
+import { Switch, Route } from "react-router-dom";
+import Home from "../src/components/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Layout>
   );
 }
 
