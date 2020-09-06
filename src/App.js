60e05eccb6d3404da87bcb2b4793bc040443f0fb
vyeung <vl.yeung@yahoo.com>
2020-09-06 18:29:22 -0400

    add /cars/:carType/:carId route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./App.css";
 
 import Layout from "../src/hoc/layout";
@@ -6,17 +6,26 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../src/components/home/Home";
 import ShowCarTypes from "../src/components/showCarTypes/showCarTypes";
 import ShowCars from "../src/components/ShowCars/ShowCars";
+import ShowACar from "./components/ShowACar/ShowACar";
 
-function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/cars/:carType" component={ShowCars} />
-        <Route path="/cars" component={ShowCarTypes} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </Layout>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    console.log("App mounted");
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/cars/:carType/:carId" component={ShowACar} />
+          <Route path="/cars/:carType" component={ShowCars} />
+          <Route path="/cars" component={ShowCarTypes} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Layout>
+    );
+  }
 }
 
 export default App;
